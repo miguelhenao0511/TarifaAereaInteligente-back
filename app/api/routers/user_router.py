16bbeae724e685a5b2b9869f1ee351309d393c7d
Miguel Henao <mhenaoh@unal.edu.co>
2023-06-27 22:26:04 -0500

    add user functionality

--- app/api/routers/user_router.py
+++ app/api/routers/user_router.py
@@ -1,11 +1,73 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
+from app.api.services import user_service
+from app.api.models.user_model import UserModel
+from passlib.context import CryptContext
 
 router = APIRouter()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/login")
-def login():
-    return {"message": "login"}
+async def login(request: Request, response: Response):
+    data = await request.json()
+    user = user_service.getByEmail(data["email"])
+    if (user != None and pwd_context.verify(data["password"], user.password)):
+        return {"message": "Login success", "user": user}
+    response.status_code = 400
+    return {"message": "Invalid user/password"}
 
 @router.post("/logout")
 def logout():
     return {"message": "logout"}
+
+
+@router.post("/user")
+async def login(request: Request, response: Response):
+    data = await request.json()
+    user = user_service.getByEmail(data["email"])
+    if (user == None):
+        new_user = UserModel(first_name = data["first_name"], last_name = data["last_name"], email = data["email"], password = pwd_context.hash(data["password"]), active_subscription = False)
+        new_user = user_service.addUser(new_user)
+        return {"message": "Create user successfully!!", "user": new_user}
+    response.status_code = 400
+    return {"message": "Email already exists!!"}
+
+@router.put("/user/{id}")
+async def login(request: Request, response: Response, id):
+    user = user_service.getById(id)
+    if (user != None):
+        data = await request.json()
+        if (data["first_name"] != user.first_name):
+            user.first_name = data["first_name"]
+        if (data["last_name"] != user.last_name):
+            user.last_name = data["last_name"]
+        if (data["email"] != user.email):
+            user.email = data["email"]
+        if (data["password"] != ""):
+            user.password = pwd_context.hash(data["password"])
+        user = user_service.updateUser(user)
+        return {"message": "Update user successfully!!", "user": user}
+        
+    response.status_code = 404
+    return {"message": "User not found"}
+
+@router.put("/user/{id}/activate_subscription")
+async def login(request: Request, response: Response, id):
+    user = user_service.getById(id)
+    if (user != None):
+        user.active_subscription = True
+        user = user_service.updateUser(user)
+        return {"message": "Activate subscription successfully!!", "user": user}
+        
+    response.status_code = 404
+    return {"message": "User not found"}
+
+@router.put("/user/{id}/deactivate_subscription")
+async def login(request: Request, response: Response, id):
+    user = user_service.getById(id)
+    if (user != None):
+        user.active_subscription = False
+        user = user_service.updateUser(user)
+        return {"message": "Activate subscription successfully!!", "user": user}
+        
+    response.status_code = 404
+    return {"message": "User not found"}
---
+++ app/api/services/user_service.py
@@ -0,0 +1,24 @@
+from app.api.conifg.database import SessionLocal
+from app.api.models.user_model import UserModel
+
+db = SessionLocal()
+
+def getById(id):
+    user = db.query(UserModel).filter(UserModel.id == id).first()
+    return user
+
+def getByEmail(email):
+    user = db.query(UserModel).filter(UserModel.email == email).first()
+    return user
+
+def addUser(user: UserModel):
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def updateUser(user: UserModel):
+    db.merge(user)
+    db.commit()
+    db.refresh(user)
+    return user
